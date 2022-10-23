9167e95b324cb241d03066b63719354cb300364f
daniel.pickett@hotmail.co.uk <40858622+dan3988@users.noreply.github.com>
2022-10-23 20:39:39 +0100

    Added filter text box

--- src/json-viewer.ts
+++ src/json-viewer.ts
@@ -5,31 +5,140 @@ HTML(document.head).append('link', {
 		href: chrome.extension.getURL("res/json.css"),
 		rel: "stylesheet"
 	}
-})
+});
 
-export function load(json: any) {
-	console.debug(json);
+const body = HTML(document.body);
 
-	const root = HTML(document.body);
-	buildUnknown(root, json);
+body.append("div", {
+	class: "header",
+	children: [
+		new ElementInit("input", {
+			class: "filter",
+			props: {
+				type: "search",
+				placeholder: "Filter"
+			},
+			events: {
+				input(e) {
+					const text = this.value.toLowerCase();
+					filters.update(text);
+				}
+			}
+		})
+	]
+});
+
+const root = body.append("div", {
+	class: "json-root"
+});
+
+interface FilterHelperContainer extends FilterHelper {
+	addTextFilter(element: undefined | HTMLElement, text: string): FilterHelper;
+	addContainer(element?: HTMLElement): FilterHelperContainer;
 }
 
-function buildPrimitive(parent: HTML, item: string, type: "string"): void;
-function buildPrimitive(parent: HTML, item: number, type: "number"): void;
-function buildPrimitive(parent: HTML, item: bigint, type: "bigint"): void;
-function buildPrimitive(parent: HTML, item: boolean, type: "boolean"): void;
-function buildPrimitive(parent: HTML, item: any, type: any): void
-function buildPrimitive(parent: HTML, item: any, type: any): void {
-	parent.append("div", {
+abstract class FilterHelper {
+	static readonly #TextFilter = class TextFilter extends FilterHelper {
+		readonly #text: string;
+
+		constructor(parent: undefined | FilterHelper, element: undefined | HTMLElement, text: any) {
+			super(parent, element);
+			this.#text = String.prototype.toLowerCase.call(text);
+		}
+
+		protected __show(filterText: string): boolean {
+			let text = this.#text;
+			if (text == "additionalProperties")
+				console.log();
+
+			return !filterText || this.#text.includes(filterText);
+		}
+	}
+
+	static readonly #ContainerFilter = class ContainerFilter extends FilterHelper implements FilterHelperContainer {
+		readonly #children: FilterHelper[];
+
+		constructor(parent: undefined | FilterHelper, element: undefined | HTMLElement) {
+			super(parent, element);
+			this.#children = [];
+		}
+
+		addTextFilter(element: undefined | HTMLElement, text: string): FilterHelper {
+			const f = new FilterHelper.#TextFilter(this, element, text);
+			this.#children.push(f);
+			return f;
+		}
+
+		addContainer(element?: undefined | HTMLElement): FilterHelperContainer {
+			const f = new FilterHelper.#ContainerFilter(this, element);
+			this.#children.push(f);
+			return f;
+		}
+
+		protected __show(filterText: string): boolean {
+			let any = false;
+			for (let child of this.#children) {
+				let shown = child.update(filterText)
+				any ||= shown;
+			}
+
+			return any;
+		}
+	}
+
+	static root(element: HTMLElement): FilterHelperContainer {
+		return new FilterHelper.#ContainerFilter(undefined, element,);
+	}
+
+	readonly #parent: undefined | FilterHelper;
+	readonly #element: undefined | HTMLElement;
+
+	constructor(parent: undefined | FilterHelper, element: undefined | HTMLElement) {
+		this.#parent = parent;
+		this.#element = element;
+	}
+
+	protected abstract __show(filterText: string): boolean;
+
+	update(filterText: string): boolean {
+		let show = this.__show(filterText);
+		let e = this.#element;
+		if (e != null)
+			e.hidden = !show;
+
+		return show;
+	}
+}
+
+let current: any;
+let filters: FilterHelperContainer;
+
+export function load(json: any) {
+	current = null;
+	filters = FilterHelper.root(root.element);
+	root.removeAll();
+	buildUnknown(root, filters, json);
+	current = json;
+}
+
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: string, type: "string"): void;
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: number, type: "number"): void;
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: bigint, type: "bigint"): void;
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: boolean, type: "boolean"): void;
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: any, type: any): void
+function buildPrimitive(parent: HTML, parentFilter: FilterHelperContainer, item: any, type: any): void {
+	const { element } = parent.append("div", {
 		class: `json-value json-${type}`,
 		children: [
 			JSON.stringify(item)
 		]
 	});
+
+	parentFilter.addTextFilter(element, item);
 }
 
-function buildKey(parent: HTML, item: string) {
-	parent.append("div", {
+function buildKey(html: HTML, item: string) {
+	html.append("div", {
 		class: `json-key`,
 		children: [
 			item
@@ -37,21 +146,21 @@ function buildKey(parent: HTML, item: string) {
 	});
 }
 
-function buildUnknown(parent: HTML, item: any): void {
+function buildUnknown(parent: HTML, parentFilter: FilterHelperContainer, item: any): void {
 	const type = typeof item;
 	switch (type) {
 		case "string":
 		case "number":
 		case "bigint":
 		case "boolean":
-			buildPrimitive(parent, item, type);
+			buildPrimitive(parent, parentFilter, item, type);
 			return;
 	}
 
 	if (Array.isArray(item)) {
-		buildArray(parent, item);
+		buildArray(parent, parentFilter, item);
 	} else {
-		buildObject(parent, Object.entries(item));
+		buildObject(parent, parentFilter, Object.entries(item));
 	}
 }
 
@@ -64,12 +173,14 @@ function buildSummary(parent: HTML, count: number, isObject: boolean): void {
 	})
 }
 
-function buildProperty(parent: HTML, key: any, value: any) {
+function buildProperty(parent: HTML<HTMLElement>, parentFilter: FilterHelperContainer, key: any, value: any) {
 	const li = parent.append("div", { class: "json-prop" });
+	const group = parentFilter.addContainer(li.element);
+	group.addTextFilter(undefined, key);
 	const type = typeof value;
 	if (type !== "object") {
 		buildKey(li, key);
-		buildPrimitive(li, value, type);
+		buildPrimitive(li, group, value, type);
 	} else {
 		if (Array.isArray(value)) {
 			buildKey(li, key);
@@ -77,7 +188,7 @@ function buildProperty(parent: HTML, key: any, value: any) {
 
 			if (value.length !== 0) {
 				buildExpander(li);
-				buildArray(li, value);
+				buildArray(li, group, value);
 			}
 		} else {
 			const entries = Object.entries(value);
@@ -86,19 +197,21 @@ function buildProperty(parent: HTML, key: any, value: any) {
 
 			if (entries.length !== 0) {
 				buildExpander(li);
-				buildObject(li, entries);
+				buildObject(li, group, entries);
 			}
 		}
 	}
 }
 
-function buildArray(parent: HTML, item: any[]): void {
+function buildArray(parent: HTML, parentFilter: FilterHelperContainer, item: any[]): void {
 	const element = parent.append("div", {
 		class: "json-container json-array",
 	});
 
+	const fh = parentFilter.addContainer(element.element);
+
 	for (let i = 0; i < item.length; i++)
-		buildProperty(element, i, item[i]);
+		buildProperty(element, fh, i, item[i]);
 }
 
 function buildExpander(parent: HTML) {
@@ -110,13 +223,15 @@ function buildExpander(parent: HTML) {
 	})
 }
 
-function buildObject(parent: HTML, entries: [string, any][]): void {
+function buildObject(parent: HTML, parentFilter: FilterHelperContainer, entries: [string, any][]): void {
 	const element = parent.append("div", {
 		class: "json-container json-object",
 	});
 
+	const fh = parentFilter.addContainer(element.element);
+
 	for (const [key, value] of entries)
-		buildProperty(element, key, value);
+		buildProperty(element, fh, key, value);
 }
 
 function toggleExpanded(this: HTMLElement) {
