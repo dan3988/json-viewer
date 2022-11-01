64f3abe41ac90b0d5dcf66515f5c759108dc0f11
daniel.pickett@hotmail.co.uk <daniel.pickett@workmobileforms.com>
2022-11-01 09:53:06 +0000

    Fixed JSON path expressions not evaluating

--- src/json-path.ts
+++ src/json-path.ts
@@ -1,4 +1,16 @@
 // @ts-ignore
 import * as jp from "../node_modules/jsonpath-plus/src/jsonpath.js";
 
+let url = chrome.extension.getURL("node_modules/vm-browserify/index.js");
+let res = await fetch(url);
+let script = await res.text();
+
+jp.JSONPath.prototype.vm = run(script);
+
+function run(__script__: string) {
+	let exports = {};
+	eval(__script__);
+	return exports;
+}
+
 export var { JSONPath, JSONPathClass }: typeof import("jsonpath-plus") = jp;
