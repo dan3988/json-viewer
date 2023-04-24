6983bed21aee529a2a1999b3759b15d007c553e9
dan3988 <daniel.pickett@hotmail.co.uk>
2023-04-24 10:30:18 +0100

    Fixed indentation

--- src/content/LoadOverlay.svelte
+++ src/content/LoadOverlay.svelte
@@ -1,6 +1,5 @@
 <script lang="ts" >
-    let elem: HTMLElement;
-	let err: string;
+	let elem: HTMLElement;
 
 	async function onYes() {
 		const res = await chrome.runtime.sendMessage({ type: "loadme" });
@@ -18,36 +17,36 @@
 	@use "../core.scss" as *;
 	@import "../globals.scss";
 
-    .root {
-        @include border-rnd;
-        @include font-elem;
+	.root {
+		@include border-rnd;
+		@include font-elem;
 
-        background-color: var(--col-bg-dk);
-        position: absolute;
-        bottom: 1rem;
-        left: 50%;
-        translate: -50% 0;
-        padding: $pad-med;
+		background-color: var(--col-bg-dk);
+		position: absolute;
+		bottom: 1rem;
+		left: 50%;
+		translate: -50% 0;
+		padding: $pad-med;
 
-        > span {
-            padding: 0 $pad-med;
-        }
-    }
+		> span {
+			padding: 0 $pad-med;
+		}
+	}
 
-    .positive {
-        background-color: #00AA00;
-        border-color: #008800;
-    }
+	.positive {
+		background-color: #00AA00;
+		border-color: #008800;
+	}
 
-    .negative {
-        background-color: #AA0000;
-        border-color: #880000;
-    }
+	.negative {
+		background-color: #AA0000;
+		border-color: #880000;
+	}
 </style>
 <template>
-    <div class="root" bind:this={elem}>
-        <span>Load JSON viewer?</span>
-        <button class="btn positive" on:click={onYes}>Yes</button>
-        <button class="btn negative" on:click={onNo}>No</button>
-    </div>
+	<div class="root" bind:this={elem}>
+		<span>Load JSON viewer?</span>
+		<button class="btn positive" on:click={onYes}>Yes</button>
+		<button class="btn negative" on:click={onNo}>No</button>
+	</div>
 </template>
