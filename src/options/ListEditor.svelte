37d95ad342d79e24dddce2e7c7a5c97d90bd70ae
dan3988 <daniel.pickett@hotmail.co.uk>
2023-05-03 11:27:42 +0100

    Change help icon color

--- src/options/ListEditor.svelte
+++ src/options/ListEditor.svelte
@@ -115,7 +115,7 @@
 	}
 
 	.btn-help {
-		@include img-btn-url("info.svg", 2px, ("default": var(--bs-blue)));
+		@include img-btn-url("info.svg", 2px, ("default": var(--bs-link-color), "hover": var(--bs-link-hover-color)));
 	}
 
 	.root {
