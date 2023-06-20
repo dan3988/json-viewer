d654b4fe43c5dc995feb40f423a2e9b61434098c
dan3988 <daniel.pickett@hotmail.co.uk>
2023-06-20 10:57:16 +0100

    Added Func helper type, add optional argument types on Action

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,12 @@
 declare type Fn<TArgs extends any[] = any[], TRet = any, TThis = any> = (this: TThis, ...args: TArgs) => TRet;
-declare type Action = () => void;
+declare type Action<T1 = never, T2 = never, T3 = never, T4 = never, T5 = never> = (...args: UntilNever<[T1, T2, T3, T4, T5]>) => void;
+declare type Func<TResult, T1 = never, T2 = never, T3 = never, T4 = never, T5 = never> = (...args: UntilNever<[T1, T2, T3, T4, T5]>) => TResult;
 declare interface Constructor<TValue, TArgs extends any[] = any[]> {
 	readonly prototype: TValue;
 	new(...args: TArgs): TValue;
 }
+
+type UntilNever<T extends any[], Prev extends any[] = []> = T extends [never, ...any[]] ? Prev : (T extends [infer Start, ...infer Rest] ? UntilNever<Rest, [...Prev, Start]> : Prev);
 
 declare type Dict<T = any> = Record<string, T>;
 
