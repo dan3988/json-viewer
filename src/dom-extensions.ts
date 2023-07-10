4e5be5e5f8369d64b7971bebf1809d3dc9b49d85
dan3988 <daniel.pickett@hotmail.co.uk>
2023-07-10 16:10:46 +0100

    Added documentation to dom event helpers

--- src/dom-extensions.ts
+++ src/dom-extensions.ts
@@ -1,8 +1,34 @@
 interface ExtendedEventTarget<Events = Dict<Event>> extends EventTarget {
+	/**
+	 * Shorthand for {@linkcode EventTarget.addEventListener}
+	 * @param type The type of event to listen for
+	 * @param handler The function to be called when the event is dispatched
+	 */
 	on<K extends keyof Events>(type: K, handler: (this: this, evt: Events[K]) => any): this;
+	/**
+	 * Shorthand for {@linkcode EventTarget.addEventListener} with `{ once: true }` passed in as the third argument.
+	 * @param type The type of event to listen for
+	 * @param handler The function to be called when the event is dispatched
+	 */
 	once<K extends keyof Events>(type: K, handler: (this: this, evt: Events[K]) => any): this;
+	/**
+	 * Shorthand for {@linkcode EventTarget.removeEventListener}
+	 * @param type The type of event to remove the event handler for
+	 * @param handler The function to remove from the list of event handlers
+	 */
 	off<K extends keyof Events>(type: K, handler: (this: this, evt: Events[K]) => any): this;
+	/**
+	 * Add an event listener for events of type {@link K}, and return a function used to unsubscribe from the event.
+	 * @param type The type of event to listen for
+	 * @param handler The function to be called when the event is dispatched
+	 * @returns A function used to unsubscribe from all events
+	 */
 	subscribe<K extends keyof Events>(type: K, handler: (this: this, evt: Events[K]) => any): Action;
+	/**
+	 * Add event listeners for multiple events, using the keys of {@link handlers}, and return a function used to unsubscribe from all events.
+	 * @param handlers A dictionary of event names and their handlers
+	 * @returns A function used to unsubscribe from all events
+	 */
 	subscribe(handlers: { [P in keyof Events]?: (this: this, evt: Events[P]) => any }): Action;
 }
 
