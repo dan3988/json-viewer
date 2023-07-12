f102135357515387fed228c155e735a72da2a7d5
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-07-12 18:46:44 +0100

    Removed render function from path editor component

--- src/viewer/JsonPathEditor.svelte
+++ src/viewer/JsonPathEditor.svelte
@@ -8,75 +8,54 @@
 </script>
 <script lang="ts">
 	import type { ViewerModel } from "../viewer-model";
-	import { createEventDispatcher, tick } from "svelte";
+	import { createEventDispatcher, onMount, tick } from "svelte";
 	import dom from "./dom-helper";
 
 	export let model: ViewerModel;
 
 	$: ({ selected } = model.bag.readables);
+	$: prop = $selected ?? model.root;
 
 	const dispatcher = createEventDispatcher<EventMap>();
 
-	let _target: HTMLElement;
+	let target: HTMLElement;
 
-	function _focus() {
-		dom.setCaret(_target, 0, true);
+	function update() {
+		target.innerText = prop.path.join("/");
 	}
 
-	function _unfocus() {
+	function unfocus() {
 		getSelection()?.removeAllRanges();
+		update();
 	}
 
 	export function focus() {
-		tick().then(_focus);
+		tick().then(() => dom.setCaret(target, 0, true));
 	}
 
-	function render(target: HTMLElement, selected: null | JsonProperty) {
-		_target = target;
+	function onFocusOut() {
+		update();
+		dispatcher("cancelled");
+	}
 
-		function update(newValue: null | JsonProperty, first?: boolean) {
-			if (first || selected !== newValue) {
-				selected = newValue;
+	function onInput(evt: Event) {
 
-				if (selected == null) {
-					target.innerText = "";
-				} else {
-					target.innerText = selected.path.join("/");
-				}
-			}
+	}
+
+	function onKeyDown(evt: KeyboardEvent) {
+		if (evt.key === "Escape") {
+			if (dispatcher("cancelled"))
+				unfocus();
+		} else if (evt.key === "Enter") {
+			evt.preventDefault();
+			const path = target.innerText;
+			const resolved = model.resolve(path);
+			if (dispatcher("finished", resolved, { cancelable: true }))
+				unfocus();
 		}
-
-		const unsub = target.subscribe({
-			focusout() {
-				dispatcher("cancelled");
-			},
-			input() {
-
-			},
-			keypress(evt) {
-				if (evt.key === "Esc") {
-					if (dispatcher("cancelled"))
-						_unfocus();
-				} else if (evt.key === "Enter") {
-					evt.preventDefault();
-					const path = _target.innerText;
-					const resolved = model.resolve(path);
-					if (dispatcher("finished", resolved, { cancelable: true }))
-						_unfocus();
-				}
-			}
-		});
-
-		update(selected, true);
-
-		return {
-			update,
-			destroy() {
-				target.innerHTML = "";
-				unsub();
-			},
-		};
 	}
+
+	onMount(update);
 </script>
 <style lang="scss">
 	@use "../core.scss" as *;
@@ -85,4 +64,12 @@
 		outline: none;
 	}
 </style>
-<div class="root" tabindex="-1" role="textbox" contenteditable="true" use:render={$selected}></div>
+<svelte:window on:selectionchange={e => e}/>
+<div class="root"
+	tabindex="-1"
+	role="textbox"
+	contenteditable="true"
+	bind:this={target}
+	on:input={onInput}
+	on:focusout={onFocusOut}
+	on:keydown={onKeyDown}/>
