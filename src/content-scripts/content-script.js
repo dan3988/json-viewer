c74b51b6d2b29e98f960ebd617569a5796e7e455
dan3988 <daniel.pickett@hotmail.co.uk>
2023-07-17 09:42:57 +0100

    Check document content is valid json on non-json mime types

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,8 +1,18 @@
-{
-	const msg = {
+(() => {
+	debugger;
+	const pre = document.querySelector("body > pre:first-child:last-child")
+	if (!pre) 
+		return;
+
+	if (document.contentType !== "application/json") {
+		const text = pre.textContent.trim();
+		const isJsonContainer = text.startsWith("{") ? text.endsWith("}") : (text.startsWith("[") && text.endsWith("]"));
+		if (!isJsonContainer)
+			return;
+	}
+
+	chrome.runtime.sendMessage({
 		type: "checkme",
 		contentType: document.contentType
-	};
-
-	chrome.runtime.sendMessage(msg);
-}
+	});
+})();
