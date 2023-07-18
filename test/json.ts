05b36502b2bb7aac6e28e3e7cef50ccf2439eeaa
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-07-18 20:25:47 +0100

    Changed titles of tests

--- test/json.ts
+++ test/json.ts
@@ -2,25 +2,25 @@ import Linq from "@daniel.pickett/linq-js";
 import json from "../src/json.js";
 
 function conversionTestCommon<T extends json.JsonToken>(value: json.JsonToken, clazz: Constructor<T>, type: keyof json.JsonTokenTypeMap, subtype: keyof json.JsonTokenSubTypeMap): asserts value is T {
-	it(`Return value of json() when called with a ${type} should return an instance of ${clazz.name}`, () => expect(value).instanceOf(clazz));
-	it(`Should have the correct type`, () => expect(value.type).eq(type));
-	it(`Should have the correct subtype`, () => expect(value.subtype).eq(subtype));
-	it(`Should return true when calling is("${type}")`, () => expect(value.is(type)).to.be.true);
-	it(`Should return true when calling is("${subtype}")`, () => expect(value.is(subtype)).to.be.true);
+	it(`Returns an instance of ${clazz.name} when calling json() with a ${type}`, () => expect(value).instanceOf(clazz));
+	it(`Has the correct type`, () => expect(value.type).eq(type));
+	it(`Has the correct subtype`, () => expect(value.subtype).eq(subtype));
+	it(`Returns true when calling is("${type}")`, () => expect(value.is(type)).to.be.true);
+	it(`Returns true when calling is("${subtype}")`, () => expect(value.is(subtype)).to.be.true);
 }
 
 function conversionTestValue(value: json.JsonToken, subtype: keyof json.JsonTokenSubTypeMap, expected: any): asserts value is json.JsonValue {
 	conversionTestCommon(value, json.JsonValue, "value", subtype);
-	it("JsonValue.value should be the value passed into json()", () => expect(value.value).eq(expected));
-	it("JsonValue.proxy should be the value passed into json()", () => expect(value.proxy).eq(expected));
-	it("JsonValue.toJSON() should return the value passed into json()", () => expect(value.toJSON()).eq(expected));
-	it("JsonValue.toString() should return the stringified value passed into json()", () => expect(value.toString()).eq(JSON.stringify(expected)));
+	it(`Has a value equal to the parameter passed into json() for the property "value"`, () => expect(value.value).eq(expected));
+	it(`Has a value equal to the parameter passed into json() for the property "proxy"`, () => expect(value.proxy).eq(expected));
+	it(`Returns a value equal to the parameter passed into json() when calling toJSON()`, () => expect(value.toJSON()).eq(expected));
+	it(`Returns a value equal to the stringified parameter passed into json() when calling toString()`, () => expect(value.toString()).eq(JSON.stringify(expected)));
 }
 
 function conversionTestContainer<T extends json.JsonContainer>(value: json.JsonToken, clazz: Constructor<T>, subtype: keyof json.JsonTokenSubTypeMap, expected: any): asserts value is T {
 	conversionTestCommon(value, clazz, "container", subtype);
-	it(clazz.name + ".toJSON() should be deeply equal to the value passed into json()", () => expect(value.toJSON()).deep.eq(expected));
-	it(clazz.name + ".toString() should return the stringified value passed into json()", () => expect(value.toString()).eq(JSON.stringify(expected)));
+	it("Returns a value that is deeply equal to the value passed into json() when calling toJSON()", () => expect(value.toJSON()).deep.eq(expected));
+	it("Returns the stringified value passed into json() when calling toString()", () => expect(value.toString()).eq(JSON.stringify(expected)));
 }
 
 describe("JSON", () => {
