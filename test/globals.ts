70fd1993f1337b840d758800bf012fac7da7a3d4
dan3988 <daniel.pickett@hotmail.co.uk>
2023-07-19 07:50:05 +0100

    Removed need for dynamic imports for rollup output

---
+++ test/globals.ts
@@ -0,0 +1,5 @@
+import chai from "chai";
+
+globalThis.expect = chai.expect;
+globalThis.assert = chai.assert;
+globalThis.should = chai.should;
--- test/main.ts
+++ test/main.ts
@@ -1,8 +1,2 @@
-/// <reference path="./index.d.ts" />
-import chai from "chai";
-
-globalThis.expect = chai.expect;
-globalThis.assert = chai.assert;
-globalThis.should = chai.should;
-
-await import("./json.js");
+import "./globals.js";
+import "./json.js";
