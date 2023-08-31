29e39d63ce7e185b12fa04c4fbcb46b174fdba47
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-08-31 16:53:18 +0100

    Fixed custom error reporter throwing error when rollup warning doesn't have a location

--- rollup-log.js
+++ rollup-log.js
@@ -1,18 +1,28 @@
-import chalk from "chalk";
 import path from "node:path";
 
-/** @type {import("rollup").WarningHandlerWithDefault} */
+function extractLocation(loc) {
+	if (loc) {
+		const { file, column, line } = loc;
+		const relative = path.relative(".", file);
+		return ` \u001b[34m${relative}:${line}:${column}\u001b[39m`;
+	} else {
+		return "";
+	}
+}
+
+/**
+ * Custom error reporter for typescript errors
+ * @type {import("rollup").WarningHandlerWithDefault}
+ */
 export function onwarn(warning, handler) {
-	if (warning.plugin !== "typescript")
+	if (warning.plugin !== "typescript" || !warning.pluginCode)
 		return handler(warning);
 
-	const { file, column, line } = warning.loc;
-	const relative = path.relative(".", file);
+	const file = extractLocation(warning.loc);
 	//remove the "@rollup/plugin-typescript TS####" prefix
 	const pluginMessage = warning.message.substring(28 + warning.pluginCode.length);
-	const message = chalk.red(warning.pluginCode) + " " + chalk.blue(`${relative}:${line}:${column}`) + " " + pluginMessage;
-	console.log(message);
-	console.log(warning.frame);
+	console.log("\u001b[31m%s\u001b[39m%s: %s", warning.pluginCode, file, pluginMessage);
+	warning.frame && console.log(warning.frame);
 }
 
 export default onwarn;
