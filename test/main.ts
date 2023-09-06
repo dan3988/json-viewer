43ad934c80802c4c82a7faf538012ac63395bd61
dan3988 <daniel.pickett@hotmail.co.uk>
2023-09-06 10:29:17 +0100

    Added tests for PropertyBag and MappedPropertyBag

--- test/main.ts
+++ test/main.ts
@@ -2,4 +2,5 @@ import "./globals.js";
 import "./json-value.js";
 import "./json-array.js";
 import "./json-object.js";
+import "./props.js";
 import "./vm.js";
---
+++ test/props.ts
@@ -0,0 +1,86 @@
+import { expect } from 'chai';
+import { MappedBagBuilder, PropertyBag, ReadOnlyPropertyBag } from '../src/prop.js'; // Update with your module path
+
+function checkEqual<T, K extends keyof T>(bag: ReadOnlyPropertyBag<T>, key: K, value: T[K]) {
+	expect(bag.getValue(key)).to.eq(value);
+	expect(bag.readables[key].value).to.eq(value);
+
+	if (bag instanceof PropertyBag)
+		checkEqual(bag.readOnly, key, value);
+}
+
+describe('PropertyBag', () => {
+	it('should initialise and update values in PropertyBag', () => {
+		const values = {
+			name: 'John',
+			age: 30,
+		};
+
+		const bag = new PropertyBag(values);
+
+		checkEqual(bag, "name", "John");
+		checkEqual(bag, "age", 30);
+
+		bag.setValue('name', 'Alice');
+		bag.setValue('age', 25);
+
+		checkEqual(bag, "name", "Alice");
+		checkEqual(bag, "age", 25);
+	});
+
+	it('should initialise and update properties using MappedPropertyBag', () => {
+		const values = {
+			firstName: 'John',
+			lastName: 'Doe',
+		};
+
+		const sourceBag = new PropertyBag(values);
+		const mappedBag = new MappedBagBuilder(sourceBag)
+			.map(["lastName", "firstName"])
+			.map(['firstName', 'lastName'], 'full_name', (firstName, lastName) => firstName + " " + lastName)
+			.build();
+			
+		checkEqual(mappedBag, "firstName", "John");
+		checkEqual(mappedBag, "lastName", "Doe");
+		checkEqual(mappedBag, "full_name", "John Doe");
+
+		sourceBag.setValue("firstName", "Alice");
+		sourceBag.setValue("lastName", "Jones");
+
+		checkEqual(mappedBag, "firstName", "Alice");
+		checkEqual(mappedBag, "lastName", "Jones");
+		checkEqual(mappedBag, "full_name", "Alice Jones");
+	});
+
+	it("Should throw an error when trying to add duplicate properties on MappedBagBuilder", () => {
+		const values = {
+			firstName: 'John',
+			lastName: 'Doe',
+		};
+
+		const sourceBag = new PropertyBag(values);
+		const builder = new MappedBagBuilder(sourceBag)
+			.map("firstName", "first_name");
+
+		expect(() => builder.map("lastName", "first_name")).throws();
+	})
+
+	it('Should fire events when changing a property', () => {
+		const values = {
+			firstName: 'John',
+			lastName: 'Doe',
+		};
+
+		let onChangeCallCount = 0;
+		
+		const bag = new PropertyBag(values);
+
+		bag.onChange(v => {
+			onChangeCallCount++;
+			expect(v).to.have.keys(["firstName"]);
+			expect(v.firstName).to.eq("Alice");
+		});
+
+		bag.setValue("firstName", "Alice");
+	});
+});
