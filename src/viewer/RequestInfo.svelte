917f2a647f33f5ce03c3f1601153a7667dd40d33
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-10-26 21:06:29 +0100

    Added copy button to header rows in HTTP request details component

--- src/viewer/RequestInfo.svelte
+++ src/viewer/RequestInfo.svelte
@@ -4,6 +4,10 @@
 	function wrapHeaders(info: undefined | DocumentRequestInfo): [title: string, headers: DocumentHeader[]][] {
 		return info ? [["Request Headers", info.requestHeaders], ["Response Headers", info.responseHeaders]] : [];
 	}
+
+	function copyHeader(key: string, value: string) {
+		return navigator.clipboard.writeText(`${key}: ${value}`);
+	}
 </script>
 <script lang="ts">
 	import type ViewerModel from "../viewer-model";
@@ -13,8 +17,10 @@
 	$: info = model.requestInfo;
 </script>
 <style lang="scss">
+	@use "src/core.scss" as *;
+
 	.root {
-		grid-template-columns: min(33%, 20rem) 1fr;
+		grid-template-columns: 1.5rem min(33%, 20rem) 1fr;
 		grid-auto-flow: row;
 		grid-column-gap: .5rem;
 
@@ -37,31 +43,58 @@
 		display: contents;
 	}
 
-	.header-key {
+	.row-key {
+		grid-column: 1 / span 2;
 		text-align: right;
 		color: var(--col-json-key-fg);
 	}
 
-	.header-val {
+	.row-val {
+		grid-column: 3 / span 1;
 		color: var(--col-json-str-fg);
+	}
+
+	.header {
+		> .row-key {
+			grid-column: 2 / span 1;
+		}
+
+		&:hover > .copy-btn {
+			opacity: 0.25;
+		}
+
+		> .copy-btn:hover {
+			opacity: 1;
+		}
+	}
+	
+	.header > .row-key {
+		grid-column: 2 / span 1;
+	}
+
+	.copy-btn {
+		@include bs-icon-btn("clipboard", 25%);
+		padding: 0.2rem;
+		transition: none;
+		opacity: 0;
 	}
 </style>
 {#if info}
 	<div class="root d-grid">
-		<div class="mono status pairs flex-column text-body-emphasis">
-			<span class="header-key">Status</span>
-			<span class="header-val">{info.statusText}</span>
-			<span class="header-key">Initiated</span>
-			<span class="header-val">{new Date(info.startTime)}</span>
-			<span class="header-key">Duration</span>
-			<span class="header-val">{info.endTime - info.startTime}</span>
+		<div class="mono status pairs flex-column">
+			<span class="row-key">Status</span>
+			<span class="row-val">{info.statusText}</span>
+			<span class="row-key">Initiated</span>
+			<span class="row-val">{new Date(info.startTime)}</span>
+			<span class="row-key">Duration</span>
+			<span class="row-val">{info.endTime - info.startTime}</span>
 		</div>
 		{#each wrapHeaders(info) as [title, values]}
 			<span class="h5 title border-bottom py-2">{title}</span>
 			<ul class="list pairs mono">
 				{#each values as [key, value]}
 					<li class="header pairs">
-						<span class="copy-btn btn btn-cust-light" role="button" title="Copy"></span>
+						<span class="copy-btn btn btn-cust-light" role="button" title="Cop Header" on:click={() => copyHeader(key, value)}></span>
 						<span class="row-key" title={key}>{key}</span>
 						<span class="row-val" title={value}>{value}</span>
 					</li>
