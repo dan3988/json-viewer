634bcb09d89979283d0224f70cbdb9e93dbd33d9
dan3988 <40858622+dan3988@users.noreply.github.com>
2025-03-03 20:42:07 +0000

    Use chrome manifest file in settings page JSON preview

--- src/options/ViewerPreview.svelte
+++ src/options/ViewerPreview.svelte
@@ -1,13 +1,28 @@
+<script lang="ts" context="module">
+	function expand(prop: json.JProperty, level: number) {
+		prop.setExpanded(true);
+
+		if (--level >= 0)
+			for (const child of prop.value)
+				expand(child, level);
+	}
+
+	function expandUp(prop: json.JProperty) {
+		while (prop = prop.parentProperty!) {
+			prop.setExpanded(true);
+		}
+	}
+</script>
 <script lang="ts">
 	import JsonProperty from "../shared/JsonProperty.svelte"
 	import ViewerModel from "../viewer-model.js";
 	import json from "../json.js";
-	import sample from "./sample.json";
 
 	export let maxIndentClass: number;
 	
-	const prop = json(sample);
-	prop.setExpanded(true, true);
+	const manifest = chrome.runtime.getManifest();
+	const prop = json(manifest);
 	const model = new ViewerModel(prop);
+	prop.setExpanded(true, true);
 </script>
 <JsonProperty indent={0} {maxIndentClass} {model} {prop}></JsonProperty>
