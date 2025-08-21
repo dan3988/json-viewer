d1b7188ce3506b7b111a3c01d6f6e13db3eaf025
dan3988 <40858622+dan3988@users.noreply.github.com>
2025-08-21 17:15:14 +0100

    Improved color scheme editor style

--- src/options/ColorSetEditor.svelte
+++ src/options/ColorSetEditor.svelte
@@ -1,54 +1,108 @@
 <script lang="ts">
-	import type { CustomSchemeColorSet, SetColors } from "./custom-scheme";
-	import type { WritableStore } from "../store";
+	import type { CustomSchemeColorSet } from "./custom-scheme";
 	import ColorEditor from "./ColorEditor.svelte";
 
+	export let title: string;
 	export let value: CustomSchemeColorSet;
 	export let previewClass: string;
 
-	let tab: 0 | 1 | 2 = 0;
-
 	$: ({ background, border, text } = value);
-	$: ({ active, def, hov, act } = [background, text, border][tab] as (SetColors & { active?: WritableStore<boolean> }));
+	$: ({ def: bgDef, hov: bgHov, act: bgAct  } = background);
+	$: ({ def: txtDef, hov: txtHov, act: txtAct, active: txtEnabled } = text);
+	$: ({ def: bdDef, hov: bdHov, act: bdAct, active: bdEnabled } = border);
+
+	$: console.log({ $bdEnabled, $txtEnabled });
 </script>
-<div class="root d-flex flex-column gap-1">
-	<div class="btn-group flex-even">
-		<button class="btn btn-base" class:active={tab === 0} on:click={() => tab = 0}>Background</button>
-		<button class="btn btn-base" class:active={tab === 1} on:click={() => tab = 1}>Text</button>
-		<button class="btn btn-base" class:active={tab === 2} on:click={() => tab = 2}>Border</button>
-	</div>
-	{#if active}
-		<div class="input-group">
-			<label class="input-group-text flex-fill align-items-start gap-1">
-				<input class="form-check-input" type="checkbox" bind:checked={$active}/>
-				Active
-			</label>
-		</div>
-	{/if}
+<div class="root gap-1">
+	<span class="title">{title}</span>
+	<span class="preview-button btn {previewClass}">Normal</span>
+	<span class="preview-button btn {previewClass} force-hover">Hover</span>
+	<span class="preview-button btn {previewClass} force-active">Active</span>
+	<span class="subtitle">Background</span>
 	<div class="input-group">
-		<span class="input-group-text">Normal</span>
-		<ColorEditor disabled={active && !$active} bind:value={$def} />
+		<ColorEditor bind:value={$bgDef} />
 	</div>
 	<div class="input-group">
-		<span class="input-group-text">Hover</span>
-		<ColorEditor disabled={active && !$active} bind:value={$hov} />
+		<ColorEditor bind:value={$bgHov} />
 	</div>
 	<div class="input-group">
-		<span class="input-group-text">Active</span>
-		<ColorEditor disabled={active && !$active} bind:value={$act} />
+		<ColorEditor bind:value={$bgAct} />
 	</div>
-	<div class="color-preview d-flex flex-even gap-1">
-		<span class="btn {previewClass}">Normal</span>
-		<span class="btn {previewClass} force-hover">Hover</span>
-		<span class="btn {previewClass} force-active">Active</span>
+	<label class="subtitle">
+		<span>Text</span>
+		<input type="checkbox" class="form-check-input" bind:checked={$txtEnabled}/>
+	</label>
+	<div class="input-group">
+		<ColorEditor disabled={!$txtEnabled} bind:value={$txtDef} />
+	</div>
+	<div class="input-group">
+		<ColorEditor disabled={!$txtEnabled} bind:value={$txtHov} />
+	</div>
+	<div class="input-group">
+		<ColorEditor disabled={!$txtEnabled} bind:value={$txtAct} />
+	</div>
+	<label class="subtitle">
+		<span>Border</span>
+		<input type="checkbox" class="form-check-input" bind:checked={$bdEnabled}/>
+	</label>
+	<div class="input-group">
+		<ColorEditor disabled={!$bdEnabled} bind:value={$bdDef} />
+	</div>
+	<div class="input-group">
+		<ColorEditor disabled={!$bdEnabled} bind:value={$bdHov} />
+	</div>
+	<div class="input-group">
+		<ColorEditor disabled={!$bdEnabled} bind:value={$bdAct} />
 	</div>
 </div>
 <style lang="scss">
+	@use "../core.scss" as *;
+
+	.root {
+		display: grid;
+		grid-template-columns: 6rem repeat(3, 1fr);
+		grid-template-rows: rpeat(4, auto);
+	}
+
+	.title, .subtitle {
+		margin: $pad-med;
+		display: flex;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 1.25rem;
+		font-weight: 500;
+		display: flex;
+		justify-content: center;
+	}
+
+	.subtitle {
+		display: flex;
+		justify-content: stretch;
+
+		@at-root label#{&} {
+			cursor: pointer;
+
+			> input[type="checkbox"] {
+				cursor: pointer;
+			}
+		}
+
+		> span {
+			flex: 1 1 0;
+		}
+	}
+
 	.input-group-text {
 		flex: 1 1 0;
 	}
 
-	.color-preview {
+	.color-input {
+		display: flex;
+	}
+
+	.preview-button {
 		pointer-events: none;
 	}
 </style>
